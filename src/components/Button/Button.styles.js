641d98193d4fbe7e230c551680f327604807b018
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2023-09-13 10:56:43 -0300

    atualizando configs

--- src/components/Button/Button.styles.js
+++ src/components/Button/Button.styles.js
@@ -1,10 +1,10 @@
 import styled from "styled-components"
 
 export const StyledButton = styled.button`
-margin: 3px 3px 3px 3px;
-width: 140px;
-height: 40px;
-border-radius: 7px;
+margin: 5px;
+width: 220px;
+height: 60px;
+border-radius: 10px;
 border: none;
 font-size: 12px;
 background-color: ${(props) => props.variant === "primary" ? "#ea4c89" : "#1e1e1e"};
--- src/components/Cart/Total/Total.styles.js
+++ src/components/Cart/Total/Total.styles.js
@@ -1,12 +1,15 @@
 import styled from "styled-components"
 
 export const StyledSection = styled.section`
+background-color: #2E2C30;
 position: fixed;
 bottom: 0;
 border-top: 1px solid black;
 display: flex;
 flex-direction: column;
 align-items: center;
+width: 30%;
+
 
 tr{
     display: flex;
@@ -18,9 +21,12 @@ td{
 }
 table{
     border-bottom: 1px solid black;
-    width: 460px;
+    width: 100%;
+    height: 100%;  
 }
 .button-components{
+    width: 100%;
+    height: 100%;
     margin-top: 10px;
 }
 `
