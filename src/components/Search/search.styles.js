f1b88426ed3fb041861e06258bc61fc1c21e9f5f
David Laurindo <45413191+DavidLaurindo@users.noreply.github.com>
2023-09-19 11:57:14 -0300

    feat(client): create menu icon navbar

--- src/components/Search/search.styles.js
+++ src/components/Search/search.styles.js
@@ -3,8 +3,8 @@ import styled from "styled-components"
 export const FormSearch = styled.form`
 border: 1px solid #E6E6E6;
 border-radius: 100px;
-width: 180px;
-height: 35px;
+width: 250px;
+height: 40px;
 display: flex;
 justify-content: center;
 align-items: center;
@@ -12,16 +12,21 @@ align-items: center;
 
 input{
     height: 20px;
-    width: 150px;
+    width: 210px;
     border: none;
     outline: 0;
-    padding-left: 6px;
+    padding-left: 10px;
     background-color: transparent;
+    font-size: 16px;
+}
+::placeholder{
+    font-size: 15px;
 }
 button{
     background-color: transparent;
     border: none;
-    font-size: 11px;
+    font-size: 13px;
+    margin-bottom: 1px;
     color: #6E6E6E;
     display: flex;
     cursor: pointer;
